650ab9da1d5d8e102276ec1ee3b202baae90f970
D3s1gn3r <darkcoder9@gmail.com>
2022-09-12 15:03:43 +0300

    face detection added

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import cv2
+import sys
+from os.path import exists
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    if len(sys.argv) > 2 and exists(sys.argv[1]):
+        imageName = sys.argv[1]
+        imageAvatarName = sys.argv[2]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        # image data
+        img = cv2.imread(imageName)
+        grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        imgHeight, imgWidth, _ = img.shape
+
+        # dataset
+        faces = cv2.CascadeClassifier('haarcascade_faces.xml')
+
+        # founded faces
+        results = faces.detectMultiScale(grayImg, scaleFactor=1.4, minNeighbors=1)
+        if (len(results)):
+            results = [results[0]]
+
+            for (x, y, w, h) in results:
+                rectangeResizeX = round(w*0.35)
+                rectangeResizeY = round(h*0.75)
+
+                if x - rectangeResizeX < 0:
+                    x1 = 0
+                else:
+                    x1 = x - rectangeResizeX
+
+                if x + w + rectangeResizeX > imgWidth:
+                    x2 = imgWidth
+                else:
+                    x2 = x + w + rectangeResizeX
+
+                if y - rectangeResizeY < 0:
+                    y1 = 0
+                else:
+                    y1 = y-rectangeResizeY
+
+                if y + h + rectangeResizeY > imgHeight:
+                    y2 = imgHeight
+                else:
+                    y2 = y + h + rectangeResizeY
+
+
+                crop = img[y1:y2, x1:x2]
+                cv2.imwrite(imageAvatarName, crop)
